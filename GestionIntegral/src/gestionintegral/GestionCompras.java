eb3880372b5ba5b6cddf27cf72188bba523964c0
joaquinseco <joaquin@villariezo.com>
2018-05-30 11:56:31 +0200

    Gestion Compras 2.1 (Soluionado Bug 001. Revisado)

--- GestionIntegral/src/gestionintegral/GestionCompras.java
+++ GestionIntegral/src/gestionintegral/GestionCompras.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class GestionCompras {
 	public static void main(String[] args) {
-		System.out.println("Gestion Compras 2.1 (Soluionado Bug 001)");
+		System.out.println("Gestion Compras 2.1 (Soluionado Bug 001. Revisado)");
 	}
 }
