53082413ff8da5351f8030b643a2ce9d962d79f6
joaquinseco <joaquin@villariezo.com>
2018-05-30 11:20:26 +0200

    Gestion Compras 1.1

--- GestionIntegral/src/gestionintegral/GestionCompras.java
+++ GestionIntegral/src/gestionintegral/GestionCompras.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class GestionCompras {
 	public static void main(String[] args) {
-		System.out.println("Gestion Compras 1.0");
+		System.out.println("Gestion Compras 1.1");
 	}
 }
